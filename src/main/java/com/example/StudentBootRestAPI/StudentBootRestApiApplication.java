9c2b864da672db7f7b0704f3e75037c9332d4856
Asendra kushwaha <asendrakush@gmail.com>
2019-05-10 20:05:02 +0530

    Initial commit

---
+++ src/main/java/com/example/StudentBootRestAPI/StudentBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentBootRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentBootRestAPI/controller/StudentControllerTest.java
@@ -0,0 +1,5 @@
+package com.example.StudentBootRestAPI.controller;
+
+public class StudentControllerTest {
+
+}
